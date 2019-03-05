7e42bd83a3813ce07c5e3fd91b44423beb5a3a49
maros <marcos.ortega94@gmail.com>
2019-03-05 11:14:18 -0300

    Inversion Of Control II. Vídeo 7

--- pildoras_spring/src/es/pildoras/IoC/DirectorEmpleado.java
+++ pildoras_spring/src/es/pildoras/IoC/DirectorEmpleado.java
@@ -18,6 +18,7 @@ public class DirectorEmpleado implements Empleados {
     @Override
     public String getTareas() {
         return "Gestionar la plantilla de la empresa";
+        
     }
     
 }
